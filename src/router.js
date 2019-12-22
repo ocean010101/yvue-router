1d5eb72b957420d3b1646ac02410cdc384da7448
ocean010101 <yangyang01.xu@outlook.com>
2019-12-22 19:10:30 +0800

    Basic
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+
+Vue.use(Router)
+
+const routes = [
+    { path: '/', name: 'home', component: Home },
+    { path: '/about', name: 'about', component: About }
+]
+const router = new Router({
+    routes // (缩写) 相当于 routes: routes
+});
+
+export default router
